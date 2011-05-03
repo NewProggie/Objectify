0766f5069b5d40ae4d3a184c2f19198655a2b33a
Kai Wolf <kai.wolf@gmail.com>
2011-05-03 12:37:48 +0000

    Titel eingefügt

--- src/de/hsrm/objectify/MainActivity.java
+++ src/de/hsrm/objectify/MainActivity.java
@@ -1,13 +1,49 @@
 package de.hsrm.objectify;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private static final String TAG = "MainActivity";
+	private Context context;
+	private Button galleryButton, howtoButton, exportButton, shareButton, cameraButton;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        context = this;
+        
+        galleryButton = (Button) findViewById(R.id.dashboard_gallery_button);
+        howtoButton = (Button) findViewById(R.id.dashboard_howto_button);
+        exportButton = (Button) findViewById(R.id.dashboard_export_button);
+        shareButton = (Button) findViewById(R.id.dashboard_share_button);
+        cameraButton = (Button) findViewById(R.id.dashboard_camera_button);
+    }
+    
+    public void buttonClick(View target) {
+    	switch (target.getId()) {
+    	case R.id.dashboard_gallery_button:
+    		Toast.makeText(context, "gallery", Toast.LENGTH_SHORT).show();
+    		break;
+    	case R.id.dashboard_howto_button:
+    		Toast.makeText(context, "howto", Toast.LENGTH_SHORT).show();
+    		break;
+    	case R.id.dashboard_export_button:
+    		Toast.makeText(context, "export", Toast.LENGTH_SHORT).show();
+    		break;
+    	case R.id.dashboard_share_button:
+    		Toast.makeText(context, "share", Toast.LENGTH_SHORT).show();
+    		break;
+    	case R.id.dashboard_camera_button:
+    		Toast.makeText(context, "camera", Toast.LENGTH_SHORT).show();
+    		break;
+    	}
     }
 }
