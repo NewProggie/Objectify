2c51dd25b09d35475b2cfdd79bdc3f2be6169706
Kai Wolf <kai.wolf@gmail.com>
2011-05-09 21:47:19 +0000

    Alle restlichen Icons fertiggestellt

--- src/de/hsrm/objectify/MainActivity.java
+++ src/de/hsrm/objectify/MainActivity.java
@@ -1,20 +1,23 @@
 package de.hsrm.objectify;
 
-import de.hsrm.objectify.camera.CameraActivity;
-import de.hsrm.objectify.gallery.GalleryActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
+import de.hsrm.objectify.camera.CameraActivity;
+import de.hsrm.objectify.gallery.GalleryActivity;
 
 public class MainActivity extends Activity {
 	
 	private static final String TAG = "MainActivity";
 	private Context context;
 	private Button galleryButton, howtoButton, exportButton, shareButton, cameraButton;
+	private ImageButton actionbarCamButton;
 	
     /** Called when the activity is first created. */
     @Override
@@ -28,6 +31,8 @@ public class MainActivity extends Activity {
         exportButton = (Button) findViewById(R.id.dashboard_export_button);
         shareButton = (Button) findViewById(R.id.dashboard_share_button);
         cameraButton = (Button) findViewById(R.id.dashboard_camera_button);
+        actionbarCamButton = (ImageButton) findViewById(R.id.actionbar_cam_button);
+        actionbarCamButton.setOnClickListener(onClickListener());
     }
     
     public void buttonClick(View target) {
@@ -50,5 +55,17 @@ public class MainActivity extends Activity {
     		startActivity(camera);
     		break;
     	}
+    }
+    
+    private OnClickListener onClickListener() {
+    	OnClickListener listener = new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent camera = new Intent(context, CameraActivity.class);
+				startActivity(camera);
+			}
+		};
+		return listener;
     }
 }
