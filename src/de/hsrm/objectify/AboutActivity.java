833634b3b0e500245a961daf1589176c3462cfd1
Kai Wolf <kai.wolf@gmail.com>
2011-05-25 13:38:04 +0000

    View noch angepasst

--- src/de/hsrm/objectify/AboutActivity.java
+++ src/de/hsrm/objectify/AboutActivity.java
@@ -6,12 +6,15 @@ import de.hsrm.objectify.ui.BaseActivity;
 
 public class AboutActivity extends BaseActivity {
 
+	TextView aboutText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		TextView aboutText = new TextView(this);
+		setContentView(R.layout.about);
+		
+		aboutText = (TextView) findViewById(R.id.about_text);
 		aboutText.setText("Copyright (c) 2011 Kai Wolf.\n All rights reserved");
-		setContentView(aboutText);
 		
 		setupActionBar(getString(R.string.about), 0);
 	}
